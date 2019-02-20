09ece7a356d689328560a149f02db820e2fb3130
tiefengeist <sahirnambiar@gmail.com>
2019-02-20 11:57:09 -0800

    Done.

--- spec/integrating_models_sinatra_walkthrough_spec.rb
+++ spec/integrating_models_sinatra_walkthrough_spec.rb
@@ -21,9 +21,9 @@ describe App do
       click_button "submit"
       expect(page.status_code).to eq(200)
       expect(page).to have_text("Number of Words: 4")
-      expect(page).to have_text("Vowels: 5")
-      expect(page).to have_text("Consonants: 10")
-      expect(page).to have_text("Most Common Letter: G, used 3 times")
+      expect(page).to have_text("Vowels: 4")
+      expect(page).to have_text("Consonants: 8")
+      expect(page).to have_text("Most Common Letter: E, used 2 times")
     end
   end
 

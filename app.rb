1e9a41ff01cc0b1e9f64fbe476d4853da8f1e4a4
github username <github email address>
2019-02-20 14:18:29 -0800

    Done.

--- app.rb
+++ app.rb
@@ -8,7 +8,19 @@ class App < Sinatra::Base
 
   post '/' do
     text_from_user = params[:user_text]
+    @length = text_from_user.split(" ").length
+    @v_count = text_from_user.downcase.scan(/[aeoui]/).count
+    @c_count = text_from_user.downcase.scan(/[bcdfghjklmnpqrstvwxyz]/).count
 
+    letter_hash = Hash.new{0}
+    i = 0
+    while i < text_from_user.length
+      char = text_from_user[i]&.upcase
+      letter_hash[char] += 1
+      i += 1
+    end
+    @letter = letter_hash.key(letter_hash.values.max)
+    @letter_count = letter_hash[@letter]
     erb :results
   end
 end

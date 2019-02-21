dd42f2fbfc06694eb3ce1b4a2da6a961cc37951b
github username <github email address>
2019-02-20 16:23:58 -0800

    Done.

--- app.rb
+++ app.rb
@@ -5,10 +5,9 @@ class App < Sinatra::Base
   get '/' do
     erb :index
   end
-
+  
   post '/' do
-    text_from_user = params[:user_text]
-
+    @analyzed_text = TextAnalyzer.new(params[:user_text])    
     erb :results
   end
 end

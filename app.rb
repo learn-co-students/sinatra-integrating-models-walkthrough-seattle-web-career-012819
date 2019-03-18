113921a401648b85bd0d13e85ce609fee4369c78
Soundarya Rajendran <soundaryarj5@gmail.com>
2019-03-18 02:00:13 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 require_relative 'config/environment'
-require_relative 'models/text_analyzer.rb'
+require_relative "models/text_analyzer.rb"
 
 class App < Sinatra::Base
   get '/' do
@@ -8,7 +8,7 @@ class App < Sinatra::Base
 
   post '/' do
     text_from_user = params[:user_text]
-
+    @text = TextAnalyzer.new(text_from_user)
     erb :results
   end
 end
